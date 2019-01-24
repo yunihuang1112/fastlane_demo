8c54bc987de1b71bd0f5318b2de74ee17db32ab1
Yuni Huang <Yuni.Huang@dlinkcorp.com>
2019-01-24 14:06:30 +0800

    Initial Commit

---
+++ FastlaneTestTests/FastlaneTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  FastlaneTestTests.swift
+//  FastlaneTestTests
+//
+//  Created by Yuni Huang on 2019/1/24.
+//  Copyright © 2019 Yuni Huang. All rights reserved.
+//
+
+import XCTest
+@testable import FastlaneTest
+
+class FastlaneTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
